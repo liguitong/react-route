ea1080bd71dd992cd285741909a47e4d25fe745d
liguitong <cangruxue@foxmail.com>
2022-01-01 20:36:04 +0800

    2 files modified, 2 added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
-import "./main.css";
+import { render } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+import './main.css';
 
-let rootElement = document.getElementById("app");
-ReactDOM.render(<App />, rootElement);
+let rootElement = document.getElementById('app');
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
